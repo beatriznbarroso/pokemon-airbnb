93b41db696b93eee6f02f59e1d7319a47c0fca8c
Beatriz Barroso <beatriznb@goparity.com>
2021-08-30 15:24:13 +0100

    adding imaged from pokemon

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :pokemons do 
     resources :bookings, only: [:create, :update, :destroy]
   end
+
+  resources :pokemons do 
+    resources :bookings, only: [:create, :update, :destroy]
+  end
 end
